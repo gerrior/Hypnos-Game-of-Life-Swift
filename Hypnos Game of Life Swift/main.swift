b8f0df924595f2745062dce94b2d3f617267227a
Mark Gerrior <github@gerrior.com>
2021-01-13 10:27:11 -0800

    GridView was an iOS UI construct. Not needed for command line version of the app.

--- Hypnos Game of Life Swift/main.swift
+++ Hypnos Game of Life Swift/main.swift
@@ -7,65 +7,6 @@
 
 import Foundation
 
-class GridView: NSObject {
-
-    // MARK: - Properties
-    var gameGrid = GameOfLife(gridSize: 25)
-    private var cellSize: Int = 15
-
-    private var timer: Timer?
-    var timeInterval = 0.25
-    var timerRunning: Bool {
-        timer == nil ? false : true
-    }
-
-    // MARK: - Actions
-
-    // MARK: - Outlets
-
-    // MARK: - Initialization
-//        let frame = CGRect(x: 0, y: 0, width: cellSize * gridSize, height: cellSize * gridSize)
-//        self.init(frame: frame)
-//        self.gameGrid = GameGrid(gridSize: gridSize)
-//        self.cellSize = cellSize
-
-    // MARK: - Public Interface
-
-    public func cancelTimer() {
-        timer?.invalidate()
-        timer = nil
-    }
-
-    public func cellTapped(at index: Int) {
-        gameGrid.cellTapped(at: index)
-    }
-
-    public func clearGrid() {
-        gameGrid.clearGrid()
-    }
-
-    @objc private func performGameTurn() {
-        self.gameGrid.performGameTurn()
-    }
-
-    public func startTimer() {
-        timer = Timer.scheduledTimer(timeInterval: timeInterval,
-                                     target: self,
-                                     selector: #selector(performGameTurn),
-                                     userInfo: nil,
-                                     repeats: true)
-    }
-
-    public func step() {
-        gameGrid.performGameTurn()
-    }
-
-    public func useExamplePattern(pattern: Patterns) {
-        gameGrid.clearGrid()
-        gameGrid.useExamplePattern(pattern: pattern)
-    }
-}
-
 let consoleIO = ConsoleIO()
 
 consoleIO.ingestCommandLine()
@@ -74,14 +15,9 @@ if path != "" {
     let file = consoleIO.openFile()
     print("Number of lines: \(file.count)")
     
-    var cells = [Cell]()
+    let game = GameOfLife(lifeFile: file)
     
-    for row in file {
-        if row.starts(with: "#") { continue }
-        
-        cells.append(Cell(coordinates: row))
-    }
-    print(cells)
+    
 } else {
     consoleIO.writeMessage("File not specifed", to: .error)
     consoleIO.printUsage()
