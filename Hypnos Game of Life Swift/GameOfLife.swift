75af15396ffcefb279402f139b57af9c62aecdb9
Mark Gerrior <github@gerrior.com>
2021-01-13 12:44:36 -0800

    Converted to dictionary but still need to deal with cells that need to be born

--- Hypnos Game of Life Swift/GameOfLife.swift
+++ Hypnos Game of Life Swift/GameOfLife.swift
@@ -23,11 +23,12 @@ public enum Patterns {
 }
 
 class GameOfLife: NSObject {
-    var cells: [Cell] = []
+    var cells = [String:Cell]()
 
     var generation = 0
     var population: Int {
-        cells.filter{ $0.state == .alive }.count
+        0
+//BUGBUG        cells.filter{ $0.state == .alive }.count
     }
 
     public init(lifeFile: [String]) {
@@ -36,7 +37,7 @@ class GameOfLife: NSObject {
         for row in lifeFile {
             if row.starts(with: "#") { continue }
             
-            cells.append(Cell(coordinates: row))
+            cells[row] = Cell(coordinates: row)
         }
         super.init()
 
@@ -54,170 +55,175 @@ class GameOfLife: NSObject {
 
         clearGrid()
 
+        var cell: Cell
+        
         switch pattern {
 
         case .behive:
-            cells.append(Cell(x: 3, y: 2))
-            cells.append(Cell(x: 4, y: 2))
+            cell = Cell(x: 3, y: 2); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 4, y: 2); cells["\(cell.x) \(cell.y)"] = cell
 
-            cells.append(Cell(x: 2, y: 3))
-            cells.append(Cell(x: 5, y: 3))
+            cell = Cell(x: 2, y: 3); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 5, y: 3); cells["\(cell.x) \(cell.y)"] = cell
 
-            cells.append(Cell(x: 3, y: 4))
-            cells.append(Cell(x: 4, y: 4))
+            cell = Cell(x: 3, y: 4); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 4, y: 4); cells["\(cell.x) \(cell.y)"] = cell
 
         case .blinker:
-            cells.append(Cell(x: 3, y: 2))
-            cells.append(Cell(x: 4, y: 2))
-            cells.append(Cell(x: 5, y: 2))
+            cell = Cell(x: 3, y: 2); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 4, y: 2); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 5, y: 2); cells["\(cell.x) \(cell.y)"] = cell
 
         case .toad:
-            cells.append(Cell(x: 4, y: 3))
-            cells.append(Cell(x: 5, y: 3))
-            cells.append(Cell(x: 6, y: 3))
+            cell = Cell(x: 4, y: 3); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 5, y: 3); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 3); cells["\(cell.x) \(cell.y)"] = cell
 
-            cells.append(Cell(x: 3, y: 4))
-            cells.append(Cell(x: 4, y: 4))
-            cells.append(Cell(x: 5, y: 4))
+            cell = Cell(x: 3, y: 4); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 4, y: 4); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 5, y: 4); cells["\(cell.x) \(cell.y)"] = cell
 
         case .beacon:
-            cells.append(Cell(x: 2, y: 2))
-            cells.append(Cell(x: 3, y: 2))
-            cells.append(Cell(x: 2, y: 3))
+            cell = Cell(x: 2, y: 2); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 3, y: 2); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 2, y: 3); cells["\(cell.x) \(cell.y)"] = cell
 
-            cells.append(Cell(x: 5, y: 4))
-            cells.append(Cell(x: 4, y: 5))
-            cells.append(Cell(x: 5, y: 5))
+            cell = Cell(x: 5, y: 4); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 4, y: 5); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 5, y: 5); cells["\(cell.x) \(cell.y)"] = cell
 
         case .pulsar:
             // Row 1
-            cells.append(Cell(x: 6, y: 2))
-            cells.append(Cell(x: 6, y: 3))
-            cells.append(Cell(x: 6, y: 4))
-            cells.append(Cell(x: 7, y: 4))
+            cell = Cell(x: 6, y: 2); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 3); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 4); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 7, y: 4); cells["\(cell.x) \(cell.y)"] = cell
 
-            cells.append(Cell(x: 12, y: 2))
-            cells.append(Cell(x: 12, y: 3))
-            cells.append(Cell(x: 12, y: 4))
-            cells.append(Cell(x: 11, y: 4))
+            cell = Cell(x: 12, y: 2); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 12, y: 3); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 12, y: 4); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 11, y: 4); cells["\(cell.x) \(cell.y)"] = cell
 
             // Row 2
-            cells.append(Cell(x: 2, y: 6))
-            cells.append(Cell(x: 3, y: 6))
-            cells.append(Cell(x: 4, y: 6))
-            cells.append(Cell(x: 4, y: 7))
+            cell = Cell(x: 2, y: 6); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 3, y: 6); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 4, y: 6); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 4, y: 7); cells["\(cell.x) \(cell.y)"] = cell
 
-            cells.append(Cell(x: 7, y: 6))
-            cells.append(Cell(x: 8, y: 6))
-            cells.append(Cell(x: 8, y: 7))
-            cells.append(Cell(x: 6, y: 7))
-            cells.append(Cell(x: 6, y: 8))
-            cells.append(Cell(x: 7, y: 8))
+            cell = Cell(x: 7, y: 6); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 8, y: 6); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 8, y: 7); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 7); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 8); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 7, y: 8); cells["\(cell.x) \(cell.y)"] = cell
 
-            cells.append(Cell(x: 10, y: 6))
-            cells.append(Cell(x: 11, y: 6))
-            cells.append(Cell(x: 10, y: 7))
-            cells.append(Cell(x: 12, y: 7))
-            cells.append(Cell(x: 11, y: 8))
-            cells.append(Cell(x: 12, y: 8))
+            cell = Cell(x: 10, y: 6); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 11, y: 6); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 10, y: 7); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 12, y: 7); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 11, y: 8); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 12, y: 8); cells["\(cell.x) \(cell.y)"] = cell
 
-            cells.append(Cell(x: 14, y: 6))
-            cells.append(Cell(x: 15, y: 6))
-            cells.append(Cell(x: 16, y: 6))
-            cells.append(Cell(x: 14, y: 7))
+            cell = Cell(x: 14, y: 6); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 15, y: 6); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 16, y: 6); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 14, y: 7); cells["\(cell.x) \(cell.y)"] = cell
 
             // Row 3
-            cells.append(Cell(x: 2, y: 12))
-            cells.append(Cell(x: 3, y: 12))
-            cells.append(Cell(x: 4, y: 12))
-            cells.append(Cell(x: 4, y: 11))
+            cell = Cell(x: 2, y: 12); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 3, y: 12); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 4, y: 12); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 4, y: 11); cells["\(cell.x) \(cell.y)"] = cell
 
-            cells.append(Cell(x: 6, y: 10))
-            cells.append(Cell(x: 7, y: 10))
-            cells.append(Cell(x: 6, y: 11))
-            cells.append(Cell(x: 8, y: 11))
-            cells.append(Cell(x: 7, y: 12))
-            cells.append(Cell(x: 8, y: 12))
+            cell = Cell(x: 6, y: 10); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 7, y: 10); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 11); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 8, y: 11); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 7, y: 12); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 8, y: 12); cells["\(cell.x) \(cell.y)"] = cell
 
-            cells.append(Cell(x: 11, y: 10))
-            cells.append(Cell(x: 12, y: 10))
-            cells.append(Cell(x: 12, y: 11))
-            cells.append(Cell(x: 10, y: 11))
-            cells.append(Cell(x: 10, y: 12))
-            cells.append(Cell(x: 11, y: 12))
+            cell = Cell(x: 11, y: 10); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 12, y: 10); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 12, y: 11); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 10, y: 11); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 10, y: 12); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 11, y: 12); cells["\(cell.x) \(cell.y)"] = cell
 
-            cells.append(Cell(x: 14, y: 11))
-            cells.append(Cell(x: 14, y: 12))
-            cells.append(Cell(x: 15, y: 12))
-            cells.append(Cell(x: 16, y: 12))
+            cell = Cell(x: 14, y: 11); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 14, y: 12); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 15, y: 12); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 16, y: 12); cells["\(cell.x) \(cell.y)"] = cell
 
             // Row 4
-            cells.append(Cell(x: 6, y: 14))
-            cells.append(Cell(x: 6, y: 15))
-            cells.append(Cell(x: 6, y: 16))
-            cells.append(Cell(x: 7, y: 14))
+            cell = Cell(x: 6, y: 14); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 15); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 16); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 7, y: 14); cells["\(cell.x) \(cell.y)"] = cell
 
-            cells.append(Cell(x: 12, y: 14))
-            cells.append(Cell(x: 12, y: 15))
-            cells.append(Cell(x: 12, y: 16))
-            cells.append(Cell(x: 11, y: 14))
+            cell = Cell(x: 12, y: 14); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 12, y: 15); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 12, y: 16); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 11, y: 14); cells["\(cell.x) \(cell.y)"] = cell
 
         case .pentadecathlon:
             // Object 1
-            cells.append(Cell(x: 5, y: 4))
-            cells.append(Cell(x: 6, y: 4))
-            cells.append(Cell(x: 7, y: 4))
-            cells.append(Cell(x: 6, y: 5))
-            cells.append(Cell(x: 6, y: 6))
-            cells.append(Cell(x: 5, y: 7))
-            cells.append(Cell(x: 6, y: 7))
-            cells.append(Cell(x: 7, y: 7))
+            cell = Cell(x: 5, y: 4); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 4); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 7, y: 4); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 5); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 6); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 5, y: 7); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 7); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 7, y: 7); cells["\(cell.x) \(cell.y)"] = cell
 
             // Object 2
-            cells.append(Cell(x: 5, y: 9))
-            cells.append(Cell(x: 6, y: 9))
-            cells.append(Cell(x: 7, y: 9))
-            cells.append(Cell(x: 5, y: 10))
-            cells.append(Cell(x: 6, y: 10))
-            cells.append(Cell(x: 7, y: 10))
+            cell = Cell(x: 5, y: 9); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 9); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 7, y: 9); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 5, y: 10); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 10); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 7, y: 10); cells["\(cell.x) \(cell.y)"] = cell
 
             // Object 3
-            cells.append(Cell(x: 5, y: 12))
-            cells.append(Cell(x: 6, y: 12))
-            cells.append(Cell(x: 7, y: 12))
-            cells.append(Cell(x: 6, y: 13))
-            cells.append(Cell(x: 6, y: 14))
-            cells.append(Cell(x: 5, y: 15))
-            cells.append(Cell(x: 6, y: 15))
-            cells.append(Cell(x: 7, y: 15))
+            cell = Cell(x: 5, y: 12); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 12); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 7, y: 12); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 13); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 14); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 5, y: 15); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 6, y: 15); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 7, y: 15); cells["\(cell.x) \(cell.y)"] = cell
 
         case .glider:
-            cells.append(Cell(x: 3, y: 2))
-            cells.append(Cell(x: 4, y: 3))
-            cells.append(Cell(x: 4, y: 4))
-            cells.append(Cell(x: 3, y: 4))
-            cells.append(Cell(x: 2, y: 4))
+            cell = Cell(x: 3, y: 2); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 4, y: 3); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 4, y: 4); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 3, y: 4); cells["\(cell.x) \(cell.y)"] = cell
+            cell = Cell(x: 2, y: 4); cells["\(cell.x) \(cell.y)"] = cell
+            
         }
 
         generation = 0
     }
 
     func cellAt(x: Int, y: Int) -> Cell? {
-        for cell in cells {
-            if cell.x == x && cell.y == y {
-                return cell
-            }
-        }
+        // Is it in the current list of live cells?
+//        let key = "\(x) \(y)"
+//        for (key, cell) in cells {
+//            if cell.x == x && cell.y == y {
+//                return cell
+//            }
+//        }
+
+        // Is it in the current list of dead adjacent cells?
         return nil
     }
 
     func performGameTurn() {
-        var cellsToKill: [Cell] = []
-        var cellsToBirth: [Cell] = []
+        var cellsToKill: [String] = []
+        var cellsToBirth = [String:Cell]()
 
-        for (index, cell) in cells.enumerated() {
-            cell.indexDuringTurn = index
-            
+        for (key, cell) in cells {
             var count = 0
             let coordinates = (x: cell.x, y: cell.y)
 
@@ -295,22 +301,21 @@ class GameOfLife: NSObject {
 
             if cell.state == .alive {
                 if count < 2 || count > 3 {
-                    cellsToKill.append(cell)
+                    cellsToKill.append(key)
                 }
             } else { // cell.state == .dead
                 if count == 3 {
-                    cellsToBirth.append(cell)
+                    cellsToBirth[key] = cell
                 }
             }
         }
 
-        for cell in cellsToKill {
-            // cell.state = .dead
-            cells.remove(at: cell.indexDuringTurn)
+        for dead in cellsToKill {
+            cells[dead] = nil
         }
 
-        for cell in cellsToBirth {
-            cells.append(cell)
+        for (key, cell) in cellsToBirth {
+            cells[key] = cell
         }
 
         generation += 1
